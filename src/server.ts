2b0bd6b3ed1192969e67e177c18877eb0ee07f3f
Sejan Miah <sejankhan931@gmail.com>
2025-12-01 01:32:59 +0400

    Post Request is working

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,21 @@ import express, {Request, Response} from "express";
 const app = express()
 const port = 5000
 
+//parser
+app.use(express.json());
+// app.use(express.urlencoded())
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('Hello, this is Sejan Miah.')
+})
+
+app.post('/', (req: Request, res:Response) => {
+    console.log(req.body);
+
+    res.status(201).json({
+        success: true,
+        message: "API is working",
+    })
 })
 
 app.listen(port, () => {
