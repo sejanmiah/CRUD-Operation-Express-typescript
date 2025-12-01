a02d9208d43e891ffe9b51e62d969e09f16a0097
Sejan Miah <sejankhan931@gmail.com>
2025-12-01 14:54:41 +0400

    Data post and Data get method

--- src/server.ts
+++ src/server.ts
@@ -50,14 +50,53 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello, this is Sejan Miah.')
 })
 
-app.post('/', (req: Request, res:Response) => {
-    console.log(req.body);
+//users CRUD
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
 
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
+      [name, email]
+    );
+    // console.log(result.rows[0]);
     res.status(201).json({
-        success: true,
-        message: "API is working",
-    })
-})
+      success: false,
+      message: "Data Instered Successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+
+// users Crud
+app.get("/users", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM users`);
+
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      datails: err,
+    });
+  }
+});
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
